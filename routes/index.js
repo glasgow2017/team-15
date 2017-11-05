3ccef75a55a8e3def452c6be5315fc588a17c475
Won Maung Thein <wonmaungthein@gmail.com>
2017-11-05 05:52:29 +0000

    Changed function style to ES6 arrow function

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get('/aboutus', (req,res,next)=> {
-  res.render ('about')
+router.get('/aboutus', (req, res, next) => {
+  res.render('about')
 })
 
 router.get('/contactus', (req, res, next) => {
